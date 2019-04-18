8509109c8265c0ae38c8ee7a5ba7cb5ff94a0d9e
jpwalker <jpw33@njit.edu>
2019-04-18 19:19:45 -0400

    Finished Multiplier and TypeLibrary
    Jean - Finished adding Multiplier 4/18/19

--- TypeLibrary/types.vhd
+++ TypeLibrary/types.vhd
@@ -4,17 +4,17 @@
 
 package types is
   -- Define chip type which uses two bits to store 3 possible integer values.
-  type chip is array(1 downto 0) of bit;
+  subtype chip is bit_vector(1 downto 0);
+  
   -- Define chip_val which is an integer limited to -1 through 1.
-  subtype chip_val is integer range -1 to 1;
-  -- Define a an array type to store the integer value of each bit contained
-  -- within chip.
-  type val_array is array(1 downto 0) of integer;
-  constant bit_idx_val : val_array := (-2, 1);
+  --subtype chip_int is integer range -1 to 1;
+
+  -- Define type which is an array of chips.  
+  type chip_array is array(natural range <>) of chip;
+  
   -- Define functions on chip.
   function "*"(a, b : chip) return chip;
-  function "+"(a, b : chip) return chip;
-  function "-"(a, b : chip) return chip;
-  function Chip2Int(a : chip) return chip_val;
-  function Int2Chip(a : chip_val) return chip;
+  function "*"(a, b : chip_array) return chip_array;
+  --function Chip2Int(a : chip) return chip_int;
+  --function Int2Chip(a : chip_int) return chip;
 end package;
--- TypeLibrary/types_bdy.vhd
+++ TypeLibrary/types_bdy.vhd
@@ -1,3 +1,31 @@
 -- types_bdy.vhd
 -- Jean P. Walker
--- Body of functions used in 
+-- Body of functions defined in types.vhd
+
+package body types is
+  function "*"(a, b : chip) return chip is
+    variable ret : chip;
+  begin
+    ret(0) := a(0) and b(0);
+    ret(1) := ret(0) and (a(1) xor b(1));
+    return ret;
+  end function;
+
+  function "*"(a, b : chip_array) return chip_array is
+    variable ret : chip_array(a'range);
+  begin
+    assert (a'left = b'left) and (a'right = b'right) report "Chip arrays should have same range." severity error;
+    for i in a'range loop
+      ret(i) := a(i) * b(i);
+    end loop;
+    return ret;
+  end function;
+
+  --function Chip2Int(a : chip) return chip_int is
+  --begin
+  --end function;
+
+  --function Int2Chip(a : chip_int) return chip is
+  --begin
+  --end function;
+end package body;
